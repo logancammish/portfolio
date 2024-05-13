ab73a3911aaef5c875923cc6491eff4dcdfb2d3c
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-05-14 09:37:29 +1200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import {
     Route,
 } from "react-router-dom";
 import Home from "./pages";
+import Redir from "./pages";
  
 function App() {
     return (
         <Router>
             <Routes>
                 <Route exact path="/portfolio" element={<Home />} />
+                <Route exact path="/" element={<Redir />} />
             </Routes>
         </Router>
+        
     );
 }
  
