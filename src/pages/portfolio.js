51c481a31737006f4e33cf7e4b361a2ab651560d
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-08-07 14:12:07 +1200

    Update portfolio.js

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,15 +4,24 @@ import { useEffect, useState } from "react";
 import { durationInMonths, durationInYears } from '@progress/kendo-date-math';
 import { SocialIcon } from 'react-social-icons';
 import Cookies from 'universal-cookie';
+import { useSpring, animated, config } from "@react-spring/web";
 const cookies = new Cookies();
 
 function Portfolio() {  
   const [isActive, setIsActive] = useState(cookies.get('DM'));
 
+  const { x } = useSpring({
+    from: { x: 0 }, // Starting value of the animated property
+    x: 1,
+    config: { duration: 1000 }, // Configuration for the animation, specifying the duration
+  });
+
   useEffect(() => {
     document.title = "Logan Cammish Portfolio";
     document.body.style.backgroundColor = !isActive ? 'rgb(249, 219, 181)' : '#000000';
   }, [])
+
+
 
   const handleClick = () => {
     setIsActive(current => !current);
@@ -20,20 +29,64 @@ function Portfolio() {
     console.log(cookies.get('DM')); 
     document.body.style.backgroundColor = isActive ? 'rgb(249, 219, 181)' : '#000000';
   };
+  
+  const [isHeld, setHeld] = useState(false);
 
+
+  useEffect(() => {
+    if (!isHeld) {
+      return;
+    }
+  
+    const timeoutId = window.setTimeout(() => {
+      console.log("Timed out")
+      setHeld(false);
+    }, 250);
+  
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [isHeld]);
+
+  const onMouseEnter = () => {
+    setHeld(true);
+  };
+  
   return (
     <div style={{
       backgroundColor: isActive ? '#171717' : 'rgba(241, 238, 238, 0.791)',
       color: isActive ? 'white' : 'rgb(36, 33, 36)'
     }}  className="App">
 
+    <animated.h1
+        style={{
+          color: isActive ? 'white' : 'rgb(36, 33, 36)',
+          opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+          transform: x
+            .to({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+            })
+            .to((x) => `scale(${x})`),
+        }}
+      >Logan Cammish      </animated.h1>
+
+
       <div style={{
-        color: isActive ? 'white' : 'rgb(36, 33, 36)'
       }}  className="container">
-        <h1 style={{
-          color: isActive ? 'white' : 'rgb(36, 33, 36)'
-        }}>Logan Cammish</h1>
-        <p>{durationInYears(new Date(2007, 12, 11), new Date())} years old | Portfolio</p>
+        <animated.p
+        style={{
+          color: isActive ? 'white' : 'rgb(36, 33, 36)',
+          opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+          transform: x
+            .to({
+              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+            })
+            .to((x) => `scale(${x})`),
+        }}
+      >{durationInYears(new Date(2007, 12, 11), new Date())} years old | Portfolio      </animated.p>
+
         <br/>
         <br/>
         <img style={{
@@ -42,10 +95,12 @@ function Portfolio() {
       </div>
       <br/>
 
-      <button  style={{
+      <animated.button onMouseEnter={onMouseEnter} style={{
+          rotate: isHeld ? -1 : 0,
+          config: config.wobbly,
           backgroundColor: isActive ? '#00000' : '',
           color: isActive ? 'white' : '',
-        }} onClick={handleClick}>dark mode toggle</button>
+        }} onClick={handleClick}>dark mode toggle</animated.button>
 
       <hr style={{
         borderColor: isActive ? 'white' : 'black'
